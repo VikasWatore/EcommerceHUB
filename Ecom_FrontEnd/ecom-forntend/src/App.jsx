b71ccb828c200b4f2f904ee350addafc36cc9e6a
VikasWatore <watorevikas@gmail.com>
2026-02-05 16:19:33 +0530

    Created Navbar

--- Ecom_FrontEnd/ecom-forntend/src/App.jsx
+++ Ecom_FrontEnd/ecom-forntend/src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 import Home from './components/home/Home';
 import Products from './components/products/Products';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/shared/Navbar';
 function App() {
 
 
   return (
     <Router>
+      <Navbar></Navbar>
       <Routes>
 
         <Route path='/' element={<Home />} />
